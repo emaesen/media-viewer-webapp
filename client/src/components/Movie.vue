d3e7c0c3dce437c498e62838517c77ae38371bcf
Edward <edward.maesen@gmail.com>
2021-04-05 14:44:08 -0700

    rename component video to movie

---
+++ client/src/components/Movie.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="movie-container">
+    <div class="title info">
+      {{ movieTitle }}
+    </div>
+    <video 
+      class="movie" 
+      controls playsinline
+      preload="metadata"
+    >
+      <source :src="movieSrc" :type="mimeType">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Movie",
+  props: ['movie'],
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    movieSrc() {
+      return this.movie.src || this.movie.path
+    },
+    mimeType() {
+      return this.movie.mime || "video/mp4"
+    },
+    movieTitle() {
+      return this.movie.title || this.movie.basename.replace(".mp4","").replace(/[-_]/g, " ")
+    }
+  },
+  methods: {
+  },
+
+}
+</script>
+
+<style lang="less" scoped>
+.title {
+  margin: 20px 0 10px 0;
+}
+.movie {
+  width: 100%;
+}
+</style>
