b924d3fa083628110978e9ce993e864a9c889bda
Edward <edward.maesen@gmail.com>
2021-04-06 09:03:13 -0700

    fix user params issue

--- server/utils/initializeMoviesDB.js
+++ server/utils/initializeMoviesDB.js
@@ -21,7 +21,7 @@ async function createMovies() {
   console.log({user})
 
   const movies = await getVideoAssets()
-  //console.log({movies})
+  //console.log(movies)
 
   movies.forEach(movie => {
     createMovie(movie, user)
@@ -30,10 +30,8 @@ async function createMovies() {
 
 async function createMovie(data, user) {
   try {
-    // for some reason, the feathers service is not correctly setting
-    // the ownerId property, so we explicitly add it here:
-    movie.ownerId = user._id
-    const movie = await app.service('movies').create(data, user)
+    //https://docs.feathersjs.com/api/services.html#params
+    const movie = await app.service('movies').create(data, {user})
     console.log("initializeMoviesDB Info: movie added: ", movie.path)
   } catch(err) {
     console.error("initializeMoviesDB Error: ", err.errorType, err.key)
