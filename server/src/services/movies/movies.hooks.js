d1a83f5ea4f46a3a2c21abff8cfe892643ea1e00
Edward <edward.maesen@gmail.com>
2021-04-19 12:09:25 -0700

    add log messages

--- server/src/services/movies/movies.hooks.js
+++ server/src/services/movies/movies.hooks.js
@@ -7,22 +7,38 @@ module.exports = {
     find: [
       hooks.restrictToOwner({
         ownerField: 'ownerId'
-      })
+      }),
+      async function(context) {
+        console.log("in movies `find` hook", context.params.query)
+        return context
+      }
     ],
     get: [
       hooks.restrictToOwner({
         ownerField: 'ownerId'
-      })
+      }),
+      async function(context) {
+        console.log("in movies `get` hook", context.params.query)
+        return context
+      }
     ],
     create: [
       hooks.associateCurrentUser({
         as: 'ownerId'
-      })
+      }),
+      async function(context) {
+        console.log("in movies `create` hook", context.data)
+        return context
+      }
     ],
     update: [
       hooks.restrictToOwner({
         ownerField: 'ownerId'
-      })
+      }),
+      async function(context) {
+        console.log("in movies `update` hook", context.data)
+        return context
+      }
     ],
     patch: [
       hooks.restrictToOwner({
@@ -32,7 +48,11 @@ module.exports = {
     remove: [
       hooks.restrictToOwner({
         ownerField: 'ownerId'
-      })
+      }),
+      async function(context) {
+        console.log("in movies `remove` hook", context.data)
+        return context
+      }
     ]
   },
 
