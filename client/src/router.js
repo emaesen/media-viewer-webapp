691c06f53dbb4e3d0c51cb8b266b942e64415c02
Edward <edward.maesen@gmail.com>
2021-04-26 16:28:20 -0700

    change default route

--- client/src/router.js
+++ client/src/router.js
@@ -1,8 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-import Register from "./views/Register.vue";
-import Login from "./views/Login.vue";
 
 import store from "./store";
 
@@ -43,7 +40,8 @@ const router = new Router({
     {
       path: "/",
       name: "home",
-      component: Home,
+      component: () =>
+        import(/* webpackChunkName: "home" */ "./views/Home.vue"),
       beforeEnter(to, from, next) {
         checkAuth(to, from, next, "movies");
       },
@@ -54,7 +52,8 @@ const router = new Router({
     {
       path: "/register",
       name: "register",
-      component: Register,
+      component: () =>
+        import(/* webpackChunkName: "register" */ "./views/Register.vue"),
       beforeEnter(to, from, next) {
         logOut(to, from, next);
       },
@@ -65,7 +64,8 @@ const router = new Router({
     {
       path: "/login",
       name: "login",
-      component: Login,
+      component: () =>
+        import(/* webpackChunkName: "login" */ "./views/Login.vue"),
       beforeEnter(to, from, next) {
         logOut(to, from, next);
       },
@@ -74,24 +74,8 @@ const router = new Router({
       }
     },
     {
-      path: "/dashboard",
-      name: "dashboard",
-      // route level code-splitting
-      // this generates a separate chunk (dashboard.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "dashboard" */ "./views/Dashboard.vue"),
-      beforeEnter: checkAuth,
-      meta: {
-        title: "Dashboard - Media Viewer"
-      }
-    },
-    {
       path: "/movies",
       name: "movies",
-      // route level code-splitting
-      // this generates a separate chunk (movies.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "movies" */ "./views/Movies.vue"),
       beforeEnter: checkAuth,
@@ -99,76 +83,8 @@ const router = new Router({
         title: "Movies - Media Viewer"
       }
     },
-    {
-      // catch all not previously defined, and redirect to root path
-      path: "*",
-      redirect: "/"
-    }
   ]
 });
 
-// Hook to set page tile and meta tags
-// (https://alligator.io/vuejs/vue-router-modify-head/)
-// usage:
-/*
-meta: {
-      title: 'My Page Title',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'My description.'
-        },
-        {
-          property: 'og:description',
-          content: 'My description'
-        }
-      ]
-    },
-*/
-// This callback runs before every route change, including on page load.
-router.beforeEach((to, from, next) => {
-  // This goes through the matched routes from last to first, finding the closest route with a title.
-  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
-  const nearestWithTitle = to.matched
-    .slice()
-    .reverse()
-    .find(r => r.meta && r.meta.title);
-
-  // Find the nearest route element with meta tags.
-  const nearestWithMeta = to.matched
-    .slice()
-    .reverse()
-    .find(r => r.meta && r.meta.metaTags);
-
-  // If a route with a title was found, set the document (page) title to that value.
-  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
-
-  // Remove any stale meta tags from the document using the key attribute we set below.
-  Array.from(document.querySelectorAll("[data-vue-router-controlled]")).map(
-    el => el.parentNode.removeChild(el)
-  );
-
-  // Skip rendering meta tags if there are none.
-  if (!nearestWithMeta) return next();
-
-  // Turn the meta tag definitions into actual elements in the head.
-  nearestWithMeta.meta.metaTags
-    .map(tagDef => {
-      const tag = document.createElement("meta");
-
-      Object.keys(tagDef).forEach(key => {
-        tag.setAttribute(key, tagDef[key]);
-      });
-
-      // We use this to track which meta tags we create, so we don't interfere with other ones.
-      tag.setAttribute("data-vue-router-controlled", "");
-
-      return tag;
-    })
-    // Add the meta tags to the document head.
-    .forEach(tag => document.head.appendChild(tag));
-
-  next();
-});
 
 export default router;
