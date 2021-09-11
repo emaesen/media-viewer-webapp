e6afaaef7bccb637a394ae6902ffb09d68377b77
Edward <edward.maesen@gmail.com>
2021-09-11 15:20:39 -0700

    add debounce utility function

---
+++ client/src/utils/helper.js
@@ -0,0 +1,21 @@
+export function debounce(func,wait,immediate){
+  //Borrowed from underscore.js.
+  //For frequently firing listener functions like scroll or resize.
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  var timeout;
+  return function() {
+    var context = this;
+    var args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
